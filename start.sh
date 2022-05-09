ec342ab7c87d4f7ba2eccdbd460111addce2adaf
Paunchybird <guillermobuendia@hotmail.es>
2022-05-09 11:21:48 +0200

    testing

--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@
 
 sudo apt-get update && sudo apt-get upgrade
 #Install docker
+echo "isolcpus=3" | sudo tee -a /boot/cmdline.txt
+sudo sed -i -e 's/dtparam=audio=on/dtparam=audio=off/' /boot/config.txt
+
 curl -sSL https://get.docker.com | sh
 
 sudo apt-get install libffi-dev libssl-dev
@@ -16,4 +19,9 @@ sudo docker pull portainer/portainer-ce:latest
 
 sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
+git clone https://github.com/Paunchybird/led-celling.git
+
+cd led-celling/
+
 sudo ./restart.sh
+
